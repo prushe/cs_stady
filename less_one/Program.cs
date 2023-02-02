afcd1ec8d2296ff2f891bedafeb911835413dbf9
prushe <alexandr.ptrshv@outlook.com>
2023-02-02 20:07:51 +0300

    add homework one and two

---
+++ less_one/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+System.Console.WriteLine("\"Hello\"");
+
+//Task Two
+int[] numsA = { 5, 2, -9 };
+int[] numsB = { 7, 10, -3 };
+int rowLen = (numsA.Length + numsB.Length) / 2;
+for (int i = 0; i < rowLen; i++)
+{
+    System.Console.Write($"{numsA[i]}  \tor {numsB[i]} \t: ");
+    if (numsA[i] > numsB[i])
+        System.Console.WriteLine(numsA[i]);
+    else
+        System.Console.WriteLine(numsB[i]);
+}
+
+System.Console.WriteLine();
+//Task Four
+int[,] numbers = new int[3, 3] {
+                    { 2, 3, 7 },
+                    { 44, 5, 78 },
+                    { 22, 3, 9 } };
+
+int rows = numbers.GetUpperBound(0) + 1;
+int columns = numbers.GetUpperBound(1) + 1;
+
+for (int i = 0; i < rows; i++)
+{
+    int maxNumInRow = 0;
+    for (int j = 0; j < columns; j++)
+    {
+        System.Console.Write($"{numbers[i, j]} \t");
+        if (maxNumInRow < numbers[i, j])
+            maxNumInRow = numbers[i, j];
+    }
+    System.Console.Write($": {maxNumInRow} \n");
+}
+
+System.Console.WriteLine();
+//Task Six
+int[] chnums = { 4, -3, 7 };
+foreach (int n in chnums)
+{
+    System.Console.Write($"{n}\t== even: ");
+    if (n % 2 == 0)
+        System.Console.Write("True \n");
+    else
+        System.Console.Write("False \n");
+}
+
+System.Console.WriteLine();
+//Task Eight
+int[] evenNumsInNums = { 5, 8 };
+foreach (int numer in evenNumsInNums)
+{
+    System.Console.Write($"in {numer} :");
+    for (int i = 2; i <= numer; i += 2)
+    {
+        System.Console.Write($"\t{i}");
+    }
+    System.Console.WriteLine();
+}
---
+++ less_two/Program.cs
@@ -0,0 +1,70 @@
+﻿// Task Teen
+int[] secNum = { 456, 782, 918 };
+foreach (int i in secNum)
+{
+    System.Console.WriteLine(i / 10 % 10);
+}
+System.Console.WriteLine();
+
+// Task thirteen
+int[] thirdNum = { 645, 78, 32679 };
+
+// option ONE
+foreach (int num in thirdNum)
+{
+    int i = num;
+    if (Convert.ToString(num).Length <= 2)
+        System.Console.WriteLine("третьей цифры нет");
+    else
+    {
+        if (Convert.ToString(i).Length > 3)
+        {
+            do
+            {
+                i = i / 10;
+            }
+            while (Convert.ToString(i).Length > 3);
+            System.Console.WriteLine(i % 10);
+        }
+        else
+        {
+            System.Console.WriteLine(i % 10);
+        }
+
+    }
+
+
+}
+System.Console.WriteLine();
+
+// option TWO
+int queue = 0;
+foreach (var num in thirdNum)
+    if (Convert.ToString(num).Length == 2)
+        System.Console.WriteLine("третьей цифры нет");
+    else
+        foreach (var i in Convert.ToString(num))
+        {
+            queue++;
+            if (queue >= 3)
+            {
+                System.Console.WriteLine(i);
+                queue = 0;
+            }
+
+        }
+System.Console.WriteLine();
+
+// task fifteen
+System.Console.Write("Количество проверок: ");
+int iter = Convert.ToInt32(Console.ReadLine());
+for (; iter >= 1; iter--)
+{
+    System.Console.Write("Порядковый номер вашего дня: ");
+    int day = Convert.ToInt32(System.Console.ReadLine());
+    if (day < 6)
+        System.Console.Write("нет \n");
+    else
+        System.Console.Write("да \n");
+    System.Console.WriteLine();
+}
