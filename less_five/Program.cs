83fcb91fd3d3c2c8d85188ca99b56947055c316c
prushe <alexandr.ptrshv@outlook.com>
2023-02-11 00:19:59 +0300

    update

--- less_five/Program.cs
+++ less_five/Program.cs
@@ -1,16 +1,4 @@
-﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
-// Напишите программу, которая покажет количество чётных чисел в массиве.
-// [345, 897, 568, 234] -> 2
-// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
-// Найдите сумму элементов, стоящих на нечётных позициях.
-// [3, 7, 23, 12] -> 19
-// [-4, -6, 89, 6] -> 0
-// Задача 38: Задайте массив вещественных чисел. 
-// Найдите разницу между максимальным и минимальным элементов массива.
-// [3 7 22 2 78] -> 76
-
-// Task Thirtyfour
-void generArray(int sizeArray, int start, int end, out int[] array)
+﻿void generArray(int sizeArray, int start, int end, out int[] array)
 {
     array = new int[sizeArray];
     var rnd = new Random();
@@ -18,6 +6,7 @@ void generArray(int sizeArray, int start, int end, out int[] array)
         array[i] = rnd.Next(start, end);
 }
 
+// Task Thirtyfour
 int arrayEvenCheck(int[] array)
 {
     int count = 0;
