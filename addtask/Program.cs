2167f46b86346487dfb668deba9ed6c7bb7ceeea
prushe <alexandr.ptrshv@outlook.com>
2023-02-11 01:00:17 +0300

    addTask

---
+++ addtask/Program.cs
@@ -0,0 +1,18 @@
+﻿void generArray(int sizeArray, int start, int end, out int[] array)
+{
+    array = new int[sizeArray];
+    var rnd = new Random();
+    for (int i = 0; i < array.Length; i++) array[i] = rnd.Next(start, end);
+}
+int[] multiArray(int[] array)
+{
+    int[] multi = new int[array.Length / 2 + array.Length % 2];
+    if (array.Length % 2 == 1) multi[multi.Length - 1] = array[multi.Length - 1];
+    for (int i = 0; i < array.Length / 2; i++) multi[i] = array[i] * array[array.GetUpperBound(0) - i];
+    return multi;
+}
+System.Console.Write("Введите размер массива:");
+generArray(Convert.ToInt32(System.Console.ReadLine()), 0, 10, out int[] array);
+System.Console.WriteLine($"[{string.Join(", ", array)}]");
+System.Console.Write("Колличество четных чисел = ");
+System.Console.WriteLine($"[{string.Join(", ", multiArray(array))}]");
