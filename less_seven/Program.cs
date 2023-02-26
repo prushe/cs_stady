66b6829d9d121e7b279b8857390ee933d6e028ea
prushe <alexandr.ptrshv@outlook.com>
2023-02-26 21:05:30 +0300

    task 47 done

--- less_seven/Program.cs
+++ less_seven/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// task Fourti seven
+double[,] GenerateArray(int m, int n)
+{
+    Random rand = new Random();
+    double[,] array = new double[m, n];
+    for (int i = 0; i <= array.GetUpperBound(0); i++)
+    {
+        for (int j = 0; j <= array.GetUpperBound(1); j++)
+        {
+            array[i, j] = rand.Next(-100, 101) / 10.0;
+        }
+    }
+    return array;
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i <= array.GetUpperBound(0); i++)
+    {
+        for (int j = 0; j <= array.GetUpperBound(1); j++)
+        {
+            System.Console.Write(array[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+double[,] myArray = GenerateArray(3, 4);
+PrintArray(myArray);
