a1945d8d6565d96ee43e6c0e020481afdacc1b9e
prushe <alexandr.ptrshv@outlook.com>
2023-02-27 18:10:20 +0300

    refact

--- less_eight/Program.cs
+++ less_eight/Program.cs
@@ -1,6 +1,5 @@
-﻿goto strt;
-// task fivetin four
-int[,] GenerateIntArray(int m, int n, int maxx)
+﻿// task fivetin four
+int[,] GenerateInt2DArray(int m, int n, int maxx)
 {
     Random rand = new Random();
     int[,] array = new int[m, n];
@@ -13,7 +12,7 @@ int[,] GenerateIntArray(int m, int n, int maxx)
     }
     return array;
 }
-void PrintIntArray(int[,] array)
+void PrintInt2DArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
@@ -49,10 +48,10 @@ int[,] Arrange(int[,] array)
     }
     return array;
 }
-int[,] myIntArray = GenerateIntArray(4, 10, 101);
-PrintIntArray(myIntArray);
-myIntArray = Arrange(myIntArray);
-PrintIntArray(myIntArray);
+int[,] myInt2DArray = GenerateInt2DArray(4, 10, 101);
+PrintInt2DArray(myInt2DArray);
+myInt2DArray = Arrange(myInt2DArray);
+PrintInt2DArray(myInt2DArray);
 System.Console.WriteLine();
 
 // task fivetin six
@@ -85,30 +84,30 @@ void MinAverageLine(int[,] array)
     int minLine = Min(averageLine);
     System.Console.WriteLine($"Строка с наименьшим средним арифметическим под №{minLine}");
 }
-myIntArray = GenerateIntArray(6, 2, 100);
-PrintIntArray(myIntArray);
-MinAverageLine(myIntArray);
+myInt2DArray = GenerateInt2DArray(6, 2, 100);
+PrintInt2DArray(myInt2DArray);
+MinAverageLine(myInt2DArray);
 System.Console.WriteLine();
 
 // task fivetin eight
-int[,] MtrixMultiply(int[,] myIntArrayOne, int[,] myIntArrayTwo)
+int[,] MtrixMultiply(int[,] myInt2DArrayOne, int[,] myInt2DArrayTwo)
 {
-    int[,] result = new int[myIntArrayOne.GetLength(0), myIntArrayTwo.GetLength(1)];
+    int[,] result = new int[myInt2DArrayOne.GetLength(0), myInt2DArrayTwo.GetLength(1)];
     for (int i = 0; i < result.GetLength(0); i++)
     {
         for (int j = 0; j < result.GetLength(1); j++)
         {
-            result[i, j] = myIntArrayOne[i, 0] * myIntArrayTwo[0, j] + myIntArrayOne[i, 1] * myIntArrayTwo[1, j];
+            result[i, j] = myInt2DArrayOne[i, 0] * myInt2DArrayTwo[0, j] + myInt2DArrayOne[i, 1] * myInt2DArrayTwo[1, j];
         }
     }
     return result;
 }
-int[,] myIntArrayOne = GenerateIntArray(2, 2, 10);
-int[,] myIntArrayTwo = GenerateIntArray(2, 2, 10);
-PrintIntArray(myIntArrayOne);
-PrintIntArray(myIntArrayTwo);
-int[,] resultIntArray = MtrixMultiply(myIntArrayOne, myIntArrayTwo);
-PrintIntArray(resultIntArray);
+int[,] myInt2DArrayOne = GenerateInt2DArray(2, 2, 10);
+int[,] myInt2DArrayTwo = GenerateInt2DArray(2, 2, 10);
+PrintInt2DArray(myInt2DArrayOne);
+PrintInt2DArray(myInt2DArrayTwo);
+int[,] resultIntArray = MtrixMultiply(myInt2DArrayOne, myInt2DArrayTwo);
+PrintInt2DArray(resultIntArray);
 System.Console.WriteLine();
 
 //task sixteen
